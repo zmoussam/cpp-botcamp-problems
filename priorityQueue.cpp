321e53f169b0d6ed36faad01c8679c4af2a25c98
Zakaria Moussammi <zakariamoussammi87@gmail.com>
2023-08-08 00:47:54 +0100

    Tue Aug  8 00:47:54 +01 2023

--- priorityQueue.cpp
+++ priorityQueue.cpp
@@ -1,2 +1,39 @@
 #include <iostream>
-#include 
+#include <queue>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, np, op, qt, value;
+    cin >> n;
+    cin >> np;
+
+    vector<priority_queue<int> > v(n);
+
+    for (int i = 0; i < np; i++)
+    {
+        cin >> op;
+        if (op == 0)
+        {
+            cin >> qt;
+            cin >> value;
+            v[qt].push(value);
+        }
+        else if (op == 1)
+        {
+            cin >> qt;
+            if (!v[qt].empty())
+                cout << v[qt].top() << '\n';
+        }
+        else if (op == 2)
+        {
+            cin >> qt;
+            if (!v[qt].empty())
+                v[qt].pop();
+        }
+    }
+}
